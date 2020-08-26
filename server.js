e94add496ce9fe058319a17383187b8edaaeca8e
Stephen Gary <sgary0@protonmail.com>
2020-08-26 14:52:57 -0400

    heroku app working

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const PORT = process.env.PORT || 3002;
 const express = require('express');
 const router = require('./api/api-router.js');
 const cors = require('cors');
-// const fs = require('fs');
-// const https = require('https');
 const server = express();
 
 server.use(cors())
@@ -15,11 +13,6 @@ server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// https.createServer({
-//   key: fs.readFileSync('./key.pem'),
-//   cert: fs.readFileSync('./cert.pem'),
-//   passphrase: 'pass'
-// }, server)
 server.listen(PORT);
 
 module.exports = server;
