9e376ba3ba854abab3502dd742103b10d90bf1c2
Stephen Gary <sgary0@protonmail.com>
2020-08-26 14:35:16 -0400

    heroku has issue h13 trying to fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const PORT = process.env.PORT || 3002;
 const express = require('express');
 const router = require('./api/api-router.js');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 const server = express();
 
 server.use(cors())
@@ -15,11 +15,11 @@ server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-https.createServer({
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem'),
-  passphrase: 'pass'
-}, server)
-.listen(PORT);
+// https.createServer({
+//   key: fs.readFileSync('./key.pem'),
+//   cert: fs.readFileSync('./cert.pem'),
+//   passphrase: 'pass'
+// }, server)
+server.listen(PORT);
 
 module.exports = server;
