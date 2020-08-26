724a32bf43000227a12abf417c60fb103aa5b64b
Stephen Gary <sgary0@protonmail.com>
2020-08-26 13:57:26 -0400

    OpenSSL fix

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 const server = require('./server.js');
-
-const PORT = process.env.PORT || 3002;
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = require('./api/api-router.js');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 const server = express();
 
 server.use(cors())
 server.use(express.json());
 server.use('/', router);
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3002;
+
+https.createServer({
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
+  passphrase: 'pass'
+}, server)
+.listen(PORT);
 
 module.exports = server;
