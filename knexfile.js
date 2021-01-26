f75e45cac96bbd11b874a157e4f4edff7d094295
stgary <sgary0@protonmail.com>
2021-01-26 13:13:15 -0500

    knexfile annex

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-const { DB_USER, DATABASE_URL, DATABASE, CLIENT, CONNECTION } = require('./config.js');
+// const { DB_USER, DATABASE_URL, DATABASE, CLIENT, CONNECTION } = require('./config.js');
 
-module.exports = {
-  development: {
-    client: CLIENT,
-    connection: { user: DB_USER, database: DATABASE }
-  },
-  production: { 
-    client: CLIENT, 
-    connection: CONNECTION ?ssl = 
-  },
-  useNullAsDefault: true,
+// module.exports = {
+//   development: {
+//     client: CLIENT,
+//     connection: { user: DB_USER, database: DATABASE }
+//   },
+//   production: { 
+//     client: CLIENT, 
+//     connection: CONNECTION ?ssl = 
+//   },
+//   useNullAsDefault: true,
 
-};
+// };
