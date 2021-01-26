a2fde3f63ab010a0ce4f8f6fc77185384706c375
stgary <sgary0@protonmail.com>
2021-01-26 08:25:51 -0500

    pg

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       database: process.env.DATABASE,
       user:     process.env.DB_USER,
